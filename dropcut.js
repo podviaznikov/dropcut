75cd41d9d74c48edc678b4d236c7fbf9415baa7b
Anton Podviaznikov <podviaznikov@gmail.com>
2011-05-10 23:21:41 +0200

    added dropcut.js

---
+++ dropcut.js
@@ -0,0 +1,24 @@
+// dropcut
+// (c) 2011 Enginimation Studio (http://enginimation.com).
+// dropcut may be freely distributed under the MIT license.
+var util = require('util'),
+    express = require('express'),
+    connect = require('connect'),
+    app = express.createServer();
+app.configure(function()
+{
+    app.use(connect.favicon(__dirname + '/public/16.png'));
+    //logger
+    app.use(express.logger());
+    //router
+    app.use(app.router);
+    //public folder for static files
+    app.use(express.static(__dirname+'/public'));
+});
+app.get('/app.mf', function(req, res)
+{
+    res.header("Content-Type", "text/cache-manifest");
+    res.sendfile(__dirname + '/app.mf');
+});
+app.listen(8085);
+util.log('started app on 8085');
